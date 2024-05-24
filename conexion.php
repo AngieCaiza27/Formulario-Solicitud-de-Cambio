47fba3b4fef447bc04876d1441b8a4d9400ceead
Angie <angiecaiza27@gmail.com>
2024-05-23 19:43:12 -0500

    Se agrego el formulario del login

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$db = "bdFormulario";
+
+$conn = new mysqli($server, $username, $password, $db);
+
+// try{
+
+//     $conexion=new PDO("mysql:host=$server,$username,$password,$db");
+//     echo "Conexion Realizada"
+
+// }catch(Exeption $error){
+//     echo $error->getMessage(); 
+// }
+?>
